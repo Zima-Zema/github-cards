48e1968af2dffc8ed45990c4cf807e33a30bcf33
Abdelazim Ali <abdelazim.ali.ali@gmail.com>
2021-09-12 17:09:51 +0200

    first commit

---
+++ src/card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Card extends React.Component {
+
+    render() {
+        return (
+            <div className='github-profile'>
+                <img alt="profile" src="http://placehold.it/75" />
+                <div className="info">
+                    <div className="name">Name here...</div>
+                </div>
+            </div>
+        )
+    }
+}
+
